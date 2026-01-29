333aa5dd185c91e0bef23b6f3d5dde2e5e44d631
johnslovakia <johnslovakia06@gmail.com>
2026-01-29 19:17:22 +0100

    Remove unused SlotHighlightSystem import

--- shared/src/main/java/cz/johnslovakia/gameapi/Shared.java
+++ shared/src/main/java/cz/johnslovakia/gameapi/Shared.java
@@ -1,7 +1,6 @@
 package cz.johnslovakia.gameapi;
 
 import cz.johnslovakia.gameapi.database.Database;
-import cz.johnslovakia.gameapi.utils.SlotHighlightSystem;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
